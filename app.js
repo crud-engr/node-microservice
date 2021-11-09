b47a74ac102d96ddb44340f3eb9c57e45c0cd66f
Abeeb Ayinla <abeebayinla@gmail>
2021-11-09 02:40:12 +0100

    added swagger configuration and logger

--- app.js
+++ app.js
@@ -1,17 +1,50 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
 const UserRoute = require('./routes/UserRoute');
 require('dotenv').config();
 
+// Swagger setup and configuration
+const options = {
+    definition: {
+      openapi: "3.0.0",
+      info: {
+        title: "Hackerbay NodeJS Microservice API",
+        version: "1.0.0",
+        description: "Node Microservice api for users"
+      },
+      contact: {
+        name: "Abeeb Ayinla",
+      },
+      servers: [{ url: "http://localhost:3000" }],
+    },
+    apis: ["./routes/*.js"]
+  }
+
 // start express app
 const app = express();
+
+// all log activities
+const log = fs.createWriteStream(path.join(__dirname, "request-activities.log"),{ flags: "a" });
+
+// swagger speficication
+const spec = swaggerJsDoc(options);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(spec));
+
+// allow cross request origin sharing on this api
 app.use(cors());
+
+// get value from the request body
 app.use(express.json());
 
-if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'));
-}
+// logger
+app.use(morgan('dev'));
+
+app.use(morgan("combined", { stream: log }));
 
 // general route handler
 app.use('/api/v1/', UserRoute);
@@ -30,3 +63,5 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`App running on port: ${PORT} in ${process.env.NODE_ENV} mode`);
 });
+
+module.exports = app;
